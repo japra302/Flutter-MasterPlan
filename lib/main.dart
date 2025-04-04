017de30a76800b76b9a2699f68f5ae10e66abfb8
habib <alghifarihabib93@gmail.com>
2025-04-04 22:54:01 +0700

    Update Praktikum 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
-import './views/plan_screen.dart';
+import 'models/data_layer.dart';
+import 'views/plan_screen.dart';
+import 'provider/plan_provider.dart';
 
-void main() => runApp(const MasterPlanApp());
+void main() {
+  runApp(const MasterPlanApp());
+}
 
 class MasterPlanApp extends StatelessWidget {
   const MasterPlanApp({super.key});
@@ -10,7 +14,10 @@ class MasterPlanApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.purple),
-      home: const PlanScreen(),
+      home: PlanProvider(
+        notifier: ValueNotifier<Plan>(const Plan()),
+        child: const PlanScreen(),
+      ),
     );
   }
 }
